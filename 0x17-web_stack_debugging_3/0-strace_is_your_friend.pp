716114f569cae91da8c4ddb217125425849f30db
Merit <meritxelladech@gmail.com>
2024-08-18 15:53:16 +0100

    Fix apache 500 internal error found in WordPress setting file due to a typo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# This file has the Puppet script to fix 500 Internal Server Error when curl -sI 127.0.0.1 is provided
+# This file has the Puppet script to prevent 500 Internal Server Error when curl -sI 127.0.0.1 is provided
 
 exec { 'correct typo in wp-settings':
     command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
