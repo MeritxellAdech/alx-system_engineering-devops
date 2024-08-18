bbccb0323341e2044221672d3478a84e26af35ea
Merit <meritxelladech@gmail.com>
2024-08-18 15:14:39 +0100

    Puppet script that fixes the 500 code returned due to a typo in wp-settings.php file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
-# Apache manifest to fix 500 Internal Server Error
+# This file has the Puppet script to fix 500 Internal Server Error when curl -sI 127.0.0.1 is provided
 
-exec { 'correct typo in wp-settings file':
+exec { 'correct typo in wp-settings':
     command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
-    file    => "/bin"
+    onlyif  => "grep 'class-wp-locale.phpp' /var/www/html/wp-settings.php",
+    path    => '/bin'
 }
