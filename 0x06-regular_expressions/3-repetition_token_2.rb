dccd50aadb27833fe7e921ea1f8ff87d3811fc55
Merit <meritxelladech@gmail.com>
2024-09-01 16:39:13 +0100

    Change in the pattern

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
-# Regular expression using the repititiion token {1, 4}
+# Regular expression using the repititiion token 1 or more of the same character
 
-regex = /hb[t]{1,4}n/
+regex = /hb[t]+n/
 # Get the command line argument
 arg = ARGV[0]
 # Check if there is any match in the argument
